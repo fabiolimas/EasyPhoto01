204d8750e5563cb925b265cd573793e7fe20192a
Fábio Lima <fabiolima01@live.com>
2026-09-02 15:41:11 -0300

    ajuste secedule

--- routes/console.php
+++ routes/console.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
-use Illuminate\Support\Facades\File;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -25,25 +25,4 @@ Artisan::command('inspire', function () {
 
 
 
-Schedule::call(function () {
 
-    $directory = storage_path('app/public/uploads');
-
-    if (!File::exists($directory)) {
-        return;
-    }
-
-    foreach (File::files($directory) as $file) {
-
-        // Somente arquivos ZIP
-        if (strtolower($file->getExtension()) !== 'zip') {
-            continue;
-        }
-
-        // Excluir ZIPs com mais de 30 minutos
-        if ($file->getMTime() < now()->subMinutes(30)->timestamp) {
-            File::delete($file->getPathname());
-        }
-    }
-
-})->everyTenMinutes();
